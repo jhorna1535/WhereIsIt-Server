9eca72b7edcddff156433c0fca23e3a2a778ef0d
jhorna1535 <151662245+jhorna1535@users.noreply.github.com>
2025-01-02 20:45:13 +0600

    Create models

---
+++ models/foundPostModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const foundPostSchema = new mongoose.Schema(
+  {
+    category: { type: String, required: true },
+    productName: { type: String, required: true },
+    color: { type: String, default: "none" },
+    brand: { type: String, default: "none" },
+    description: { type: String, default: "none" },
+    possibleLocation: { type: String, required: true },
+    possibleDate: { type: String, required: true },
+    image: String,
+    type: { type: String, default: "found" },
+    status: { type: String, default: "Pending" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("FoundPost", foundPostSchema);
---
+++ models/lostPostModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const lostPostSchema = new mongoose.Schema(
+  {
+    category: { type: String, required: true },
+    productName: { type: String, required: true },
+    color: { type: String, default: "none" },
+    brand: { type: String, default: "none" },
+    description: { type: String, default: "none" },
+    possibleLocation: { type: String, required: true },
+    possibleDate: { type: String, required: true },
+    image: String,
+    type: { type: String, default: "lost" },
+    status: { type: String, default: "Pending" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("LostPost", lostPostSchema);
