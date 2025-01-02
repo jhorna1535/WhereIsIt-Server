a8428f6eb98a18bef2430b06285b652a123d58cc
jhorna1535 <151662245+jhorna1535@users.noreply.github.com>
2025-01-02 20:27:55 +0600

    Error connecting to MongoDB

--- index.js
+++ index.js
@@ -15,9 +15,10 @@ mongoose
     useUnifiedTopology: true,
   })
   .then(() => {
-    
+    console.log("Connected to MongoDB");
   })
-  ({
-
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1); // Exit the process with a failure code
   });
 
